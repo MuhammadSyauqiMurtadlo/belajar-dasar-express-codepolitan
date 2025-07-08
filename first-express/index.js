cb1cef019d5f04e2cfb6cfd55cf989abdf4e3645
Syauqi <sauki084@gmail.com>
2025-07-09 06:09:09 +0700

    Create route & downgrade express

--- first-express/index.js
+++ first-express/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const app = express();
 
+// app.use('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Home Page');
+});
+
+app.get('/cats', (req, res) => {
+  res.send('Meow!');
+});
+
+app.post('/cats', (req, res) => {
+  res.send('Cat created!!!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About this app');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact us at contact@example.com');
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('Page not found');
 });
 
 app.listen(3000, () => {
